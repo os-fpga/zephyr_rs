cc6d50e2c749ecfe85bc312a6a54be82682ba06e
junaidaslamRS <junaid.aslam@rapidsilicon.com>
2024-04-23 22:34:16 +0500

    Virgo soc DTS

---
+++ include/zephyr/dt-bindings/pinctrl/rapidsi_virgo_pinctrl.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_RAPIDSI_VIRGO_PINCTRL_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_RAPIDSI_VIRGO_PINCTRL_H_
+
+/**
+ * @brief Pin modes
+ */
+
+#define PIN_FN_MAIN 0x0
+#define PIN_FN_FPGA 0x1
+#define PIN_FN_ALT 0x2
+#define PIN_FN_DEBUG 0x3
+#define PIN_FN_UNDEFINED 0x4
+
+/**
+ * @brief Pin Pull Status Bit Values
+ */
+
+#define PIN_PULL_STATUS_DIS 0x0
+#define PIN_PULL_STATUS_EN 0x1
+
+/**
+ * @brief Pin Pull Direction Bit Values
+ */
+
+#define PIN_PULL_DIR_DOWN 0x0
+#define PIN_PULL_DIR_UP 0x1
+
+#endif
