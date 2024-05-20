6c9baace757fb3109c2cb75e1019efc30af2a39c
junaidaslamRS <junaid.aslam@rapidsilicon.com>
2024-05-20 12:28:49 +0500

    Added Vendor Prefix

--- boards/rapidsilicon/virgo_tc_eval_soc_proto/board.cmake
+++ boards/rapidsilicon/virgo_tc_eval_soc_proto/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd "--use-elf" "--gdb-init=target extended-remote 10.0.8.101:3333" "--gdb-port=3333")
+board_runner_args(openocd "--use-elf" "--gdb-init=target extended-remote 10.0.8.102:3333" "--gdb-port=3333")
 set(OPENOCD_FLASH "debug")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,13 @@
  */
 
 #include <stdio.h>
-
+volatile int I_am_imp = 0;
+volatile int counter = 100;
 int main(void)
 {
+	while(I_am_imp < counter) {
+		I_am_imp++;
+	}
 	printf("Hi Rahul we just printed Hello World to our %s\n", CONFIG_BOARD_TARGET);
 
 	return 0;
