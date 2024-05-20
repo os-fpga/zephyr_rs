c5ac69ecc4053847dcc2e2402461898dc4cfc1ad
junaidaslamRS <junaid.aslam@rapidsilicon.com>
2024-05-20 19:50:28 +0500

    k_msleep not working

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -11,9 +11,14 @@ static volatile int JATW = 0;
 static volatile const int counter = 100;
 int main(void)
 {
+	printf("Getting into Main Loop\n");
 	while(JATW < counter) {
 		JATW++;
 		printf("%d - Hello %s\n", JATW, CONFIG_BOARD_TARGET);
+		k_msleep(1000);
+	}
+	while(true) {
+		printf("Out of the Main Loop\n");
 		k_msleep(1000);
 	}
 
