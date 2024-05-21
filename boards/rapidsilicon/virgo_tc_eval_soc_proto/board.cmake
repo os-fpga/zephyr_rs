8a52f406bd2761e4974c299e4fa2f7609dcc220b
junaidaslamRS <junaid.aslam@rapidsilicon.com>
2024-05-21 18:58:03 +0500

    Further improvement in printing. delay still not working

--- boards/rapidsilicon/virgo_tc_eval_soc_proto/board.cmake
+++ boards/rapidsilicon/virgo_tc_eval_soc_proto/board.cmake
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd "--use-elf" "--gdb-init=target extended-remote 10.0.8.102:3333" "--gdb-port=3333")
-set(OPENOCD_FLASH "debug")
+board_runner_args(
+                    openocd 
+                    "--cmd-load=load"
+                    "--use-elf"  
+                    "--elf-file=/home/junaid/zephyrproject/zephyr/build/zephyr/zephyr.elf" 
+                    "--gdb-init=target extended-remote 10.0.8.102:3333"
+                 )
+set(OPENOCD_FLASH "load")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -8,18 +8,16 @@
 #include <zephyr/kernel.h>
 
 static volatile int JATW = 0;
-static volatile const int counter = 100;
+static volatile int counter = 100;
 int main(void)
 {
 	printf("Getting into Main Loop\n");
-	while(JATW < counter) {
-		JATW++;
-		printf("%d - Hello %s\n", JATW, CONFIG_BOARD_TARGET);
-		k_msleep(1000);
-	}
+
 	while(true) {
-		printf("Out of the Main Loop\n");
-		k_msleep(1000);
+		printf("%d - Infinite Main Loop %s\n", JATW++, CONFIG_BOARD_TARGET);
+		for(unsigned int i = 0; i < 500000; i++) {
+			counter++;
+		} counter = 0;
 	}
 
 	return 0;
