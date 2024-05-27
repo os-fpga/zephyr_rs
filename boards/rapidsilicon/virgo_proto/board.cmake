ffe1ada42c93d2bc21ea941119110863ada6be11
junaidaslamRS <junaid.aslam@rapidsilicon.com>
2024-05-27 16:21:47 +0500

    renaming and fixing

--- boards/rapidsilicon/virgo_proto/board.cmake
+++ boards/rapidsilicon/virgo_proto/board.cmake
@@ -9,5 +9,5 @@ board_runner_args(
                     "--elf-file=/home/junaid/zephyrproject/zephyr/build/zephyr/zephyr.elf" 
                     "--gdb-init=target extended-remote 10.0.8.102:3333"
                  )
-set(OPENOCD_FLASH "load")
+set(OPENOCD_FLASH "flash")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
